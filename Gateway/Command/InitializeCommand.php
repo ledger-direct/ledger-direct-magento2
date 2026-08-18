277e6df134c1fcf06df2b7d41e89d52979280faf
AlexanderBuzz <102560752+AlexanderBuzz@users.noreply.github.com>
2026-08-18 09:06:09 +0200

    Replace deprecated AbstractMethod payment methods with Payment\Method\Adapter - wire xrp_payment and xrpl_token_payment through Adapter virtualTypes in di.xml with a minimal InitializeCommand, removing the deprecated AbstractMethod subclasses.

---
+++ Gateway/Command/InitializeCommand.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace Hardcastle\LedgerDirect\Gateway\Command;
+
+use Magento\Payment\Gateway\CommandInterface;
+use Magento\Payment\Gateway\Data\PaymentDataObjectInterface;
+use Magento\Sales\Model\Order;
+
+/**
+ * Moves the order to pending_payment on checkout placement and suppresses the new-order email,
+ * since XRPL settlement is confirmed asynchronously via {@see \Hardcastle\LedgerDirect\Service\OrderPaymentService}.
+ */
+class InitializeCommand implements CommandInterface
+{
+    public function execute(array $commandSubject)
+    {
+        /** @var PaymentDataObjectInterface $paymentDataObject */
+        $paymentDataObject = $commandSubject['payment'];
+        $order = $paymentDataObject->getPayment()->getOrder();
+        $order->setCanSendNewEmailFlag(false);
+
+        $orderState = Order::STATE_PENDING_PAYMENT;
+        $orderStatus = $order->getConfig()->getStateDefaultStatus($orderState);
+        $comment = __('The customer was redirected for payment processing. The payment is pending.');
+        $order->setState($orderState)->addStatusToHistory($orderStatus, $comment, false);
+    }
+}
